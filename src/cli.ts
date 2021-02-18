4cf4cf88a450e9636a5279b7e066b07f7c81d7ce
Steve Tomlin <steveltomlin@gmail.com>
2021-02-18 19:17:15 +0000

    adding argument collection

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,29 @@
+type TKeyString = { [key: string]: string };
 
+type TsplitArgIntoObj = (item: string) => TKeyString | null;
+const splitArgIntoObj: TsplitArgIntoObj = item => {
+  const arr = item.split('=');
+  return {
+    [arr[0]]: arr[1]
+  };
+};
 
-type Tcli = (args: {x: true}) => string;
+type TgetArgs = (args: string[]) => TKeyString;
+const getArgs: TgetArgs = args =>
+  args.reduce(
+    (accum, cur) => ({
+      ...accum,
+      ...((cur.indexOf('--') === 0) ? splitArgIntoObj(cur.substr(2)) : {})
+    }),
+    {} as TKeyString
+  );
 
+type Tcli = (args: string[]) => string;
 const cli: Tcli = args => {
+  const objArgs = getArgs(args);
   console.log('args = ', args);
+  console.log('objArgs = ', objArgs);
   return 'hello world';
-}
+};
 
-
-export {cli};
+export { cli };
